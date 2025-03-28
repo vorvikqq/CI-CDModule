60e472fd564fc7380e211351dcd736e44fbdd79d
vorvikqq <delodonn@ukr.net>
2025-03-28 15:49:48 +0200

    add tests for read_population_data func

--- tests/test_read_population_data.py
+++ tests/test_read_population_data.py
@@ -1,5 +1,5 @@
 import pytest
-from main import read_population_data 
+from main import read_population_data
 
 @pytest.fixture
 def valid_data_file(tmp_path):
@@ -18,3 +18,20 @@ def invalid_numbers_file(tmp_path):
     file = tmp_path / "invalid_numbers.txt"
     file.write_text("USA, 98x33517, 331002651\nCanada, 9984670, not_a_number\n")
     return file
+
+
+@pytest.mark.parametrize("file_fixture, expected", [
+    ("valid_data_file", [("USA", 9833517.0, 331002651), ("Canada", 9984670.0, 37742154)])
+])
+def test_read_population_data_valid(file_fixture, expected, request):
+    file_path = request.getfixturevalue(file_fixture)
+    assert read_population_data(file_path) == expected
+
+@pytest.mark.parametrize("file_fixture", [
+    "invalid_format_file",
+    "invalid_numbers_file"
+])
+def test_read_population_data_invalid(file_fixture, request):
+    file_path = request.getfixturevalue(file_fixture)
+    with pytest.raises(ValueError):
+        read_population_data(file_path)
